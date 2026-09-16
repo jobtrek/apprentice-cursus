e7b4b4858342975399974037c1a3d6480ed56197
thomaslucking <telucking@gmail.com>
2026-09-16 10:52:16 +0200

    fix: removed logo properly

--- resources/js/layouts/auth/AuthSimpleLayout.vue
+++ resources/js/layouts/auth/AuthSimpleLayout.vue
@@ -1,6 +1,4 @@
 <script setup lang="ts">
-import { home } from '@/routes';
-import { Link } from '@inertiajs/vue3';
 
 defineProps<{
     title?: string;
@@ -15,12 +13,6 @@ defineProps<{
         <div class="w-full max-w-sm rounded-lg border bg-card p-6 shadow-md md:p-10">
             <div class="flex flex-col gap-8">
                 <div class="flex flex-col items-center gap-4">
-                    <Link
-                        :href="home()"
-                        class="flex flex-col items-center gap-2 font-medium"
-                    >
-                                                <span class="sr-only">{{ title }}</span>
-                    </Link>
                     <div class="space-y-2 text-center">
                         <h1 class="text-xl font-medium">{{ title }}</h1>
                         <p class="text-muted-foreground text-center text-sm">
