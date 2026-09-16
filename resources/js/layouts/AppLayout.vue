d4a20d5342e0d636382833cfe44e6b8eecd1da82
Thomas Lucking <128782962+ThomasLucking@users.noreply.github.com>
2026-09-16 10:13:31 +0200

    feat: fixed navbar (#159)
    * feat: fixed navbar
    
    * feat: added mobile support

--- resources/js/layouts/AppLayout.vue
+++ resources/js/layouts/AppLayout.vue
@@ -1,12 +1,12 @@
 <script setup lang="ts">
-import MainNavbar from "@/layouts/MainNavbar.vue";
+import MainNavbar from '@/layouts/MainNavbar.vue';
 </script>
 
 <template>
-    <div class="bg-background min-h-svh flex flex-row">
+    <div class="bg-background flex min-h-svh flex-col">
         <MainNavbar />
 
-        <main class="flex flex-1 flex-col justify-center items-center">
+        <main class="flex flex-1 flex-col items-center justify-center">
             <slot />
         </main>
     </div>
