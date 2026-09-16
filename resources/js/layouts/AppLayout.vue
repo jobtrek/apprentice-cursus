c04601252f5f1b9e69fe5b0c915725ac0f4c7acb
thomaslucking <telucking@gmail.com>
2026-09-16 08:30:30 +0200

    feat(ui): updated the color palette
    - moved navbar to the side instead of the top
    - used lucide react icons

--- resources/js/layouts/AppLayout.vue
+++ resources/js/layouts/AppLayout.vue
@@ -4,7 +4,7 @@ import MainNavbar from "@/layouts/MainNavbar.vue";
 </script>
 
 <template>
-    <div class="bg-background min-h-svh flex flex-col">
+    <div class="bg-background min-h-svh flex flex-row">
         <MainNavbar />
 
         <main class="flex flex-1 flex-col justify-center items-center">
