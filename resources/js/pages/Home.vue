ff63cd39dac63664788f9ae4b437a38e3417a849
thomaslucking <telucking@gmail.com>
2026-09-16 13:03:29 +0200

    chore: removed useless profile views

--- resources/js/pages/Home.vue
+++ resources/js/pages/Home.vue
@@ -1,7 +1,5 @@
 <script setup lang="ts">
-import { Head, Link } from "@inertiajs/vue3";
-import { Button } from "@/components/ui/button";
-import { edit as editProfile } from "@/routes/profile";
+import { Head } from "@inertiajs/vue3";
 </script>
 
 <template>
@@ -9,9 +7,5 @@ import { edit as editProfile } from "@/routes/profile";
 
     <div class="flex flex-col items-start gap-4">
         <h1 class="text-xl font-semibold">You're logged in</h1>
-
-        <Button as-child data-test="profile-link">
-            <Link :href="editProfile()">Go to your profile</Link>
-        </Button>
     </div>
 </template>
