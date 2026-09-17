dbbd71e141462f1b93d352e528f13194f8f1bcc6
FrstF4ll <207869736+FrstF4ll@users.noreply.github.com>
2026-09-17 13:30:42 +0200

    feat: rebuild breadcrumb subtitle

--- resources/js/components/gradeList/GradeHeader.vue
+++ resources/js/components/gradeList/GradeHeader.vue
@@ -9,10 +9,11 @@ const props = defineProps<{
 
 <template>
     <header class="mb-8">
-        <p class="text-base text-muted-foreground">Mes apprentis · {{ apprenticeName }}</p>
-        <p class="text-base text-muted-foreground mt-1">{{ filiere }} · {{ cohort }}</p>
-        <div class="flex flex-row items-center gap-3 mt-4">
+
+        <div class="flex flex-col gap-3 mt-4">
             <h1 class="text-4xl font-bold">{{ pageTitle }}</h1>
+            <h2 class="text-base text-muted-foreground">
+                Mes apprentis · {{ cohort }} · {{ filiere }} · {{ apprenticeName  }}</h2>
         </div>
     </header>
 </template>
